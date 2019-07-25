a3258b9e30faac00ae52085f52cfd991e1b3f6d4
Matthew <mattghanno@gmail.com>
2019-07-25 16:15:51 +1000

    reads each row of csv into a list and creates a new csv that those lists will be written into

--- CSVchanger.py
+++ CSVchanger.py
@@ -1,10 +1,28 @@
 import csv
 
+x = 0
+guidList = []
+fNameList = []
+sNameList = []
+
 with open('patients.csv') as csv_file:
-    csvReader = csv.DictReader(csv_file)
+    csvReader = csv.reader(csv_file)
 
     #next(csvReader) # skips the header of the csv
 
-    for line in csvReader:
-        print(line['doctor'])
+    for row in csvReader:
+        guidList.append(row[0])
+        fNameList.append(row[1])
+        sNameList.append(row[2])
+        x = x + 1
+
+print(x)
+print(guidList)
+print(fNameList)
+print(sNameList)
+
+with open('new.csv', 'w') as newCsv:
+    csvWriter = csv.writer(newCsv)
+
+
 

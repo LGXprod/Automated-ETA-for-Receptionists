2f61f84c575136c02bebcfdbc69dc4c7fbb9f8fd
Matthew <mattghanno@gmail.com>
2019-07-25 16:41:24 +1000

    creates new.csv with random times of arrive for patients

--- CSVchanger.py
+++ CSVchanger.py
@@ -1,9 +1,8 @@
 import csv
+import random
 
 x = 0
-guidList = []
-fNameList = []
-sNameList = []
+csvList = []
 
 with open('patients.csv') as csv_file:
     csvReader = csv.reader(csv_file)
@@ -11,18 +10,30 @@ with open('patients.csv') as csv_file:
     #next(csvReader) # skips the header of the csv
 
     for row in csvReader:
-        guidList.append(row[0])
-        fNameList.append(row[1])
-        sNameList.append(row[2])
+        csvList.append(row)
         x = x + 1
 
+csv_file.close()
+
 print(x)
-print(guidList)
-print(fNameList)
-print(sNameList)
+
+csvList[0].append("hour")
+csvList[0].append("minutes")
+
+for y in range(1, x):
+    randomHour = random.randint(1, 13)
+    randomMin = random.randint(1, 60)
+    csvList[y].append(randomHour)
+    csvList[y].append(randomMin)
+
+print(csvList)
+
+#for y in range(0, x):
+
 
 with open('new.csv', 'w') as newCsv:
     csvWriter = csv.writer(newCsv)
+    csvWriter.writerows(csvList)
 
 
 

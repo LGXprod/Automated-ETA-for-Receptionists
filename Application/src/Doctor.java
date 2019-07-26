13bfaf8f3adf7efc04b07f5f1c8b62e3175a1f68
Matthew <mattghanno@gmail.com>
2019-07-26 22:25:22 +1000

    creation of the java application that will estimate the times (only includes the doctor object so far)

---
+++ Application/src/Doctor.java
@@ -0,0 +1,14 @@
+public class Doctor {
+
+    String fName;
+    String sName;
+    double avgWaitTime;
+
+    public Doctor(String doctorFName, String doctorSName, double doctorWaitTime){
+        fName = doctorFName;
+        sName = doctorSName;
+        avgWaitTime = doctorWaitTime;
+
+    }
+
+}
